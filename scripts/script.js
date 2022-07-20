54b63441f51e5ef0040e734974f78ec0bf5c838b
Emaneyluos <benxxix@gmail.com>
2022-07-20 16:08:08 +0200

    Update script.js
    Minor refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -11,16 +11,20 @@ var ctx = canvas.getContext("2d");
 var canvasWidth = 1280;
 var canvasHeight = 720;
 
+var barIniX = 540;
+var barIniY = 690;
 var barX = 540;
 var barY = 690;
+var barWidth = 200;
+var barHeight = 20;
+var barSpeed = 15;
 
-ctx.fillRect(barX, barY, 200, 20);
+ctx.fillRect(barX, barY, barWidth, barHeight);
 
 document.addEventListener('keydown', keyIsPress);
 
 function keyIsPress (e)
 {
-    //39 right // 37 left
     if (e.keyCode == 39){
         moveRight (ctx);
     }
@@ -34,15 +38,15 @@ function keyIsPress (e)
 
 function moveRight ()
 {
-    ctx.clearRect(0, 690, 1280, 30);
-    barX += 10;
-    ctx.fillRect(barX, barY, 200, 20);
+    ctx.clearRect(0, barIniY, canvasWidth, (canvasHeight - barHeight)); //Remove the bar
+    barX += barSpeed; //Deplace the X
+    ctx.fillRect(barX, barY, barWidth, barHeight); //Redraw the bar
     
 }
 
 function moveLeft ()
 {
-    ctx.clearRect(0, 690, 1280, 30);
-    barX -= 10;
-    ctx.fillRect(barX, barY, 200, 20);
+    ctx.clearRect(0, barIniY, canvasWidth, (canvasHeight - barHeight)); //Remove the bar
+    barX -= barSpeed; //Deplace the X
+    ctx.fillRect(barX, barY, barWidth, barHeight); //Redraw the bar
 }
